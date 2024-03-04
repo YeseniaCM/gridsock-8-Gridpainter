7181089890f8d7c06ab556f558c80c4cb05c1a79
YeseniaCM <yeseniacamilla@hotmail.com>
2024-03-04 22:23:53 +0100

    :sparkles: feat(Client) Loginform

---
+++ client/src/getLogin.js
@@ -0,0 +1,28 @@
+export function getLogIn (email, password, message) {
+    let checkUser = {userEmail: email, userPassword: password}
+
+    fetch("http://localhost:3000/users/login", {
+            method: "POST",
+            headers: {
+                "content-Type": "application/json"
+            },
+            body: JSON.stringify(checkUser)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log("Användare:", data);
+            
+            if (data.message) {
+
+                message.textContent = data.message
+                setTimeout( ()=> {
+                 message.textContent = '';
+                } , 2000)
+            }else{
+                formDiv.innerHTML = '';
+                let setUser = JSON.stringify(data)
+                localStorage.setItem('user', setUser)
+                //  logOutBtn()
+            }
+        })
+}
---
+++ client/src/printLogin.js
@@ -0,0 +1,37 @@
+
+export function printLogInForm() {
+    let loginForm = document.createElement('div');
+    loginForm.setAttribute('class', 'login-form')
+
+    loginForm.innerHTML = '';
+    let heading = document.createElement('h1')
+    heading.textContent = 'Logga in'
+     
+    let inputEmail = document.createElement('input');
+    inputEmail.placeholder = 'Email';
+
+    let inputPassword = document.createElement('input');
+    inputPassword.classList.add("input-password")
+    inputPassword.type = 'password';
+    inputPassword.placeholder = 'Lösenord';
+
+    let message = document.createElement('p');
+
+    let signInBtn = document.createElement('button');
+    signInBtn.textContent = 'Logga in'
+
+    let createNewUser = document.createElement('a');
+    createNewUser.textContent = 'Create new account'
+
+    signInBtn.addEventListener('click', () => {
+        getLogIn(inputEmail.value, inputPassword.value, message)
+    })
+
+    createNewUser.addEventListener('click', () => {
+        // funktionen för att ligga in ska in hära
+    })
+    loginForm.append(heading, inputEmail, inputPassword, message, signInBtn, createNewUser)
+    app.appendChild(loginForm);
+}
+
+export default printLogInForm;
