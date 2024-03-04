f527c8f565bddf81e4b69feda15ccd51e0c881f2
YeseniaCM <yeseniacamilla@hotmail.com>
2024-03-04 17:12:50 +0100

    :sparkles: feat(users.js) login endpoint

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const connection = require('./lib/conn.js');
+const cors = require('cors');
 
 
 connection.connect(function(err){
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 
 app.use('/', indexRouter);
