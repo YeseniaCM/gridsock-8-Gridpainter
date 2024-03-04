d0c09a98a8778ee133ff108d0cfff0ff0cf3dc89
Ala Hojat Jalali <alahojatjalali@gmail.com>
2024-03-04 22:18:14 +0100

    :sparkles: feat(users.js) add endpoint for creating new user

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const connection = require('../lib/conn.js')
+const { randomUUID } = require('crypto');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+// Create a new user
+router.post('/add', function(req, res) {
+  let userName = req.body.userName;
+  let userEmail = req.body.userEmail;
+  let userPassword = req.body.userPassword;
+  let userId = randomUUID();
+  
+  let sql = "INSERT into users (userId, userName, userEmail, userPassword) VALUES (?, ?, ?, ?)";
+  let values = [userId, userName, userEmail, userPassword];
+
+  connection.query(sql, values, (err, data) => {
+    if (err) console.log("err", err);
+    res.json({ message: "Your account has been created"});
+  })
+})
 
 module.exports = router;
