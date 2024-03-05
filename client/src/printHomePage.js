8d45e50aeea25cb6158a7447dd8f50f1d150c8fd
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-03-05 21:11:30 +0100

    :sparkles: feat(client) homepage

--- client/src/printHomePage.js
+++ client/src/printHomePage.js
@@ -1,19 +1,44 @@
 import { loginForm } from './printLogin.js'
+// import printlogoutbtn
+//import printloginbtn
 
 export function printHomePage() {
     loginForm.innerHTML = ''
 
+    let instructionArray = [
+        'When 4 players has joined the game will start.',
+        'You´ll get a 5 second glimpse of a picture',
+        'Each person will get assigned a colour',
+        'To paint, press the box you want to color',
+        'you all have 10 minutes to complete the task',
+    ]
     let homeHeading = document.createElement('h1');
     homeHeading.textContent = 'Can you gridpaint?';
+    homeHeading.setAttribute('class', 'home-heading')
 
     let instructionsDiv = document.createElement('div')
+    instructionsDiv.setAttribute('class', 'instructions-div')
 
-    let instructions = document.createElement('p');
-    instructions.textContent = 'When 4 players has joined the game will start. You´ll get a 5 second glimpse of a picture, each person will get assigned a colour. To paint, press the box you want to color, you all have 10 minutes to complete the task. Can you do it ?'
+    let instructionsUL = document.createElement('ul');
+     
+    instructionArray.forEach(inst => {
+        let instructions = document.createElement('li');
+        instructions.setAttribute('class', 'inst-li')
+        console.log(inst)
+        instructions.textContent += inst;
+        instructionsUL.append(instructions)
+    })
 
+    let instructionQuote = document.createElement('p');
+    instructionQuote.textContent = 'Can you do it ?'
+    instructionQuote.setAttribute('class', 'inst-quote')
 
-    instructionsDiv.appendChild(instructions)
+    let circleDiv = document.createElement('div');
+    circleDiv.setAttribute('class', 'circle-div');
 
-    loginForm.append(logOutBtn(), homeHeading,instructionsDiv, startGameBtn)
+    printLogoutBtn()
+    instructionsDiv.append(instructionsUL, instructionQuote)
+    loginForm.append( homeHeading, circleDiv, instructionsDiv )
+    printStartGameBtn()
 
 }
