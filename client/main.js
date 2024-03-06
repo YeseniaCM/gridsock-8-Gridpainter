82ef2cd561ddd6245efb3e86d654bc8211dab9e7
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-03-06 08:39:54 +0100

    :sparkles: feat(client) go Home button

--- client/main.js
+++ client/main.js
@@ -2,4 +2,17 @@ import './src/styles/style.css'
 import {printLogInForm } from './src/printLogin.js'
 
 
-printLogInForm() 
+if(localStorage.getItem('user')) {
+    printHomePage()
+} else {
+    printLogInForm() 
+}
+
+
+
+
+
+
+/** Button to go back to the homepage
+ * import { goHomeBtn } from './src/printGoHomeBtn.js'
+ * goHomeBtn()*/
