d3cc7d7f6f952bebc4d0ece34285191388a33864
Ala Hojat Jalali <alahojatjalali@gmail.com>
2024-03-06 15:41:20 +0100

    :sparkles: feat(grid) add randomised function to print image-array
    Co-authored-by: Yesenia <YeseniaCM@users.noreply.github.com>, Cecilia <CeciliaL1@users.noreply.github.com>

--- client/main.js
+++ client/main.js
@@ -1,21 +1,25 @@
 import './src/styles/style.css'
+import './src/styles/grid.css'
 import {printLogInForm } from './src/printLogin.js'
 
 import { printPaintOnGrid } from './src/printPaintOnGrid.js'
 
 import { printHomePage } from './src/printHomePage.js'
 
+import { originalImages } from './src/originalImages.js'
+
+import { paintAndPrintImage } from './src/originalImages.js'
 
 if(localStorage.getItem('user')) {
     printHomePage()
-} else {
+ } else {     
     printLogInForm() 
 }
 
 
+paintAndPrintImage();
 
-
-
+console.log("Här är vår array", originalImages);
 
 /** Button to go back to the homepage
  * import { goHomeBtn } from './src/printGoHomeBtn.js'
