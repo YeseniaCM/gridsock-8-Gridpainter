f60f5b4354e20e51d25e151485dab38c7bef3275
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-03-14 21:14:42 +0100

    :bug: only save one image

--- client/src/PlayerAddingImage.js
+++ client/src/PlayerAddingImage.js
@@ -12,17 +12,20 @@ let players = usersWithName.map(user => user.userName)
             gridImage: uncoloredGrid
          }
 
-
-         fetch("http://localhost:3000/images/add", {
-             method: "POST",
-            headers: {
-                 "Content-Type": "application/json"
-             },
-             body: JSON.stringify(sendImage)
-         })
-         .then(res => res.json())
-         .then(data => {
-            printResultPage(data, roomInput, image, uncoloredGrid)
-         });
+         if (usersWithName.length === 4) {
+            
+            fetch("http://localhost:3000/images/add", {
+                method: "POST",
+               headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(sendImage)
+            })
+            .then(res => res.json())
+            .then(data => {
+               printResultPage(data, roomInput, image, uncoloredGrid)
+            });
+        }
+   
 
 }
